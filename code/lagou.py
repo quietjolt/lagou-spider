6085d0d2b0c6ec09869536dae6697e974d6078a0
quietjolt <quietjolt@163.com>
2018-04-19 11:23:43 +0800

    第二次上传,添加了建表的代码

--- code/lagou.py
+++ code/lagou.py
@@ -3,12 +3,72 @@
 
 #依赖
 	#pip3 install requests
-	#pip3 install lxml
 	#pip3 install pymysql
+	#pip3 install pyquery
 
-#代码执行位置
-	# /www/web/www_chinashouzhi_com/public_html/spider/spider3
+#执行方式
+#		python3 lagou.py
+	
 
+
+#建表的指令
+'''
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyId` bigint(4) DEFAULT NULL,
+  `positionName` varchar(64) DEFAULT NULL,
+  `workYear` varchar(16) DEFAULT NULL,
+  `education` varchar(16) DEFAULT NULL,
+  `jobNature` varchar(16) DEFAULT NULL,
+  `positionId` bigint(4) DEFAULT NULL,
+  `companyShortName` varchar(16) DEFAULT NULL,
+  `createTime` varchar(38) DEFAULT NULL,
+  `score` bigint(4) DEFAULT NULL,
+  `city` varchar(16) DEFAULT NULL,
+  `salary` varchar(16) DEFAULT NULL,
+  `approve` bigint(4) DEFAULT NULL,
+  `positionAdvantage` varchar(128) DEFAULT NULL,
+  `financeStage` varchar(16) DEFAULT NULL,
+  `industryField` varchar(16) DEFAULT NULL,
+  `companySize` varchar(18) DEFAULT NULL,
+  `companyLabelList` varchar(128) DEFAULT NULL,
+  `publisherId` bigint(4) DEFAULT NULL,
+  `district` varchar(16) DEFAULT NULL,
+  `companyLogo` varchar(106) DEFAULT NULL,
+  `positionLables` varchar(128) DEFAULT NULL,
+  `industryLables` varchar(128) DEFAULT NULL,
+  `businessZones` varchar(128) DEFAULT NULL,
+  `hitags` varchar(128) DEFAULT NULL,
+  `resumeProcessRate` bigint(4) DEFAULT NULL,
+  `resumeProcessDay` bigint(4) DEFAULT NULL,
+  `companyFullName` varchar(28) DEFAULT NULL,
+  `imState` varchar(16) DEFAULT NULL,
+  `lastLogin` bigint(4) DEFAULT NULL,
+  `explain` varchar(128) DEFAULT NULL,
+  `adWord` bigint(4) DEFAULT NULL,
+  `plus` varchar(128) DEFAULT NULL,
+  `pcShow` bigint(4) DEFAULT NULL,
+  `appShow` bigint(4) DEFAULT NULL,
+  `deliver` bigint(4) DEFAULT NULL,
+  `gradeDescription` varchar(128) DEFAULT NULL,
+  `promotionScoreExplain` varchar(128) DEFAULT NULL,
+  `firstType` varchar(16) DEFAULT NULL,
+  `secondType` varchar(16) DEFAULT NULL,
+  `isSchoolJob` bigint(4) DEFAULT NULL,
+  `subwayline` varchar(16) DEFAULT NULL,
+  `stationname` varchar(16) DEFAULT NULL,
+  `linestaion` varchar(256) DEFAULT NULL,
+  `longitude` varchar(20) DEFAULT NULL,
+  `latitude` varchar(18) DEFAULT NULL,
+  `formatCreateTime` varchar(16) DEFAULT NULL,
+  `advantage` text,
+  `describe` text,
+  `address` text,
+  `lagou_src` varchar(128) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+
+'''
 
 #导入需要的库 - 标准库
 import re       #本模块提供了类似于Perl的正则表达式匹配操作。要匹配的模式和字符串可以是Unicode字符串以及8位字符串。
@@ -43,7 +103,8 @@ page_max = 30
 
 db_host = "localhost"
 db_user = "root"
-db_password = "123456"
+#此处的密码为了安全因素没写, 请在使用的时候采用自己的密码
+db_password = ""
 db_name = "zhtest"
 db_port = 3306
 db_charset = 'utf8'
@@ -374,7 +435,6 @@ def get_list_page():
 		time.sleep(1)
 
 	pass 	#函数结束
-
 
 
 get_list_page()
